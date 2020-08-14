4168b3f75e8883c01e9da755482a317ffc97ceaf
Artem <temaspsxxx@gmail.com>
2020-08-14 20:05:16 +0300

    Create components and add logic

---
+++ src/components/history/History.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import HistoryItem from './HistoryItem'
+
+function History({ transactions }) {
+  return (
+    <section className="history">
+      {!!transactions.length && <h3>История расходов</h3>}
+      <ul className="history__list">
+        {transactions.map(item => <HistoryItem key={item.id} transaction={item}/>)}
+      </ul>
+    </section>
+  )
+}
+
+export default History;
---
+++ src/components/history/HistoryItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HistoryItem({transaction}) {
+  return (
+    <li 
+      className={`history__item ${transaction.isAdd ? 'history__item-plus' : 'history__item-minus'}`}>
+      {transaction.description}
+        <span className="history__money">{transaction.amount} ₽</span>
+        <button className="history__delete">x</button>
+    </li>
+  );
+}
+
+export default HistoryItem;
